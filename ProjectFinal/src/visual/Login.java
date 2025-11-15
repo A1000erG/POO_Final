22835f787575eba224ae6ebf3a2abca99d7269e5
Yodariz Mejias <Yodariz Mejias@LAPTOP-4JL62FEC>
2025-11-14 21:30:03 -0400

    Modificaciones en el login (Sin terminar)

--- ProjectFinal/src/visual/Login.java
+++ ProjectFinal/src/visual/Login.java
@@ -4,9 +4,13 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 public class Login extends JFrame {
 
@@ -21,6 +25,7 @@ public class Login extends JFrame {
 			public void run() {
 				try {
 					Login frame = new Login();
+					//frame.setUndecorated(true);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -45,10 +50,22 @@ public class Login extends JFrame {
 	    panelIzquierdo.setBackground(Color.WHITE);
 	    panelIzquierdo.setLayout(new BorderLayout());
 	    contentPane.add(panelIzquierdo, BorderLayout.CENTER);
+	    
+	    // TÍTULO "INICIAR SESIÓN"
+        JLabel lblTitulo = new JLabel("INICIAR SESIÓN");
+        lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+        lblTitulo.setFont(new Font("SansSerif", Font.BOLD, 32));
+
+        JPanel panelTitulo = new JPanel();
+        panelTitulo.setBackground(Color.WHITE);
+        panelTitulo.setBorder(new EmptyBorder(80, 0, 40, 0));
+        panelTitulo.add(lblTitulo);
+
+        panelIzquierdo.add(panelTitulo, BorderLayout.NORTH);
 
 	    // ------------------ PANEL DERECHO (IMAGEN / LOGO) -------------------
 	    JPanel panelDerecho = new JPanel();
-	    panelDerecho.setBackground(new Color(0, 155, 198));
+	    panelDerecho.setBackground(new Color(22,163, 74));
 	    panelDerecho.setPreferredSize(new Dimension(420, 0));
 	    contentPane.add(panelDerecho, BorderLayout.EAST);
 		
