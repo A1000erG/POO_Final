402497c8f87e5cd37607a85e5cded7009d2b2318
Yodariz Mejias <Yodariz Mejias@LAPTOP-4JL62FEC>
2025-11-15 20:26:44 -0400

    Corrección de warnings

--- ProjectFinal/src/visual/Principal.java
+++ ProjectFinal/src/visual/Principal.java
@@ -1,6 +1,5 @@
 package visual;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
@@ -11,8 +10,6 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
